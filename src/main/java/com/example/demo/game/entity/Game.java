b5fd16ceaead5e041aa851ee6feb9d7a0649664b
Yoonchulchung <yoonchul005@gmail.com>
2025-11-23 06:16:32 +0900

    feat#27 : game entity 생성

---
+++ src/main/java/com/example/demo/game/entity/Game.java
@@ -0,0 +1,29 @@
+package com.example.demo.game.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Table(name="game")
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="icon", nullable = false)
+    private String icon;
+
+    @Column(name="content", nullable = false)
+    private String content;
+
+    @Column(name="content_num", nullable = false)
+    private Integer content_num;
+
+    @Column(name="location", nullable = false)
+    private Boolean location;
+}
