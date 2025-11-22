ae3b94f139dddc37a0b1bf9d328c51f10a738abf
Yoonchulchung <yoonchul005@gmail.com>
2025-11-23 05:47:02 +0900

    chore#7 : swagger 수정

--- src/main/java/com/example/demo/global/response/ApiResponse.java
+++ src/main/java/com/example/demo/global/response/ApiResponse.java
@@ -2,12 +2,25 @@ package com.example.demo.global.response;
 
 import com.example.demo.global.exception.ErrorMessage;
 import com.example.demo.global.exception.ErrorType;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 
-public record ApiResponse<T>(ResultType result, T data, ErrorMessage error) {
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record ApiResponse<T>(
+    @Schema(description = "응답 결과", example = "SUCCESS")
+    ResultType result,
+    
+    @Schema(description = "응답 데이터")
+    T data,
+    
+    @Schema(hidden = true)  // Swagger에서 숨김
+    ErrorMessage error
+) {
 
     public static <T> ApiResponse<T> success() {
         return new ApiResponse<>(ResultType.SUCCESS, null, null);
     }
+    
     public static <S> ApiResponse<S> success(S data) {
         return new ApiResponse<>(ResultType.SUCCESS, data, null);
     }
@@ -15,6 +28,4 @@ public record ApiResponse<T>(ResultType result, T data, ErrorMessage error) {
     public static ApiResponse<?> error(ErrorType error) {
         return new ApiResponse<>(ResultType.ERROR, null, new ErrorMessage(error));
     }
-
 }
-
